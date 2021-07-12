43919369758b5ccc4fb65a745a7f302cebd5464d
litus <litulandio@gmail.com>
2021-07-12 18:49:57 +0200

    install axios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import './assets/tailwindcss/main.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).use(router).mount("#app");
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount("#app")
