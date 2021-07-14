f35af7d56e6128f436c70cd62e10ec284f5f492c
litus <litulandio@gmail.com>
2021-07-14 03:04:13 +0200

    install vuex

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from "./router";
 import './assets/tailwindcss/main.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import store from './store/index.js';
+
 
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.use(VueAxios, axios)
 app.mount("#app")
